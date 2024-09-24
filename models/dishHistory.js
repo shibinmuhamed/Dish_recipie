8a6c551d2d9e15738eb53f7ff172368a378f917c
muhamed shibin <mhsibin916@gmail.com>
2024-09-24 18:01:09 +0530

    initial commit

---
+++ models/dishHistory.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+// History schema to store the entire state of the dish
+const dishHistorySchema = new Schema({
+  dishId: { type: Schema.Types.ObjectId, ref: "Dish", required: true },
+  name: { type: String, required: true }, 
+  ingredients: [
+    {
+      ingredient: { type: Schema.Types.ObjectId, ref: "Ingredient" }, 
+      quantity: { type: Number, required: true },
+    },
+  ],
+  status: { type: String, enum: ['cooking started', 'cooking ended'], required: true }, // New field
+   updatedAt: { type: Date, default: Date.now }, 
+});
+
+module.exports = mongoose.model("DishHistory", dishHistorySchema);
